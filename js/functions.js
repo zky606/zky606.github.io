6ac346430483ac3f5a5db79ce328cc3480d9495c
zky606 <2214836047@qq.com>
2021-06-18 12:49:46 +0800

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -94,7 +94,7 @@ function startHeartAnimation() {
 
 function timeElapse(date){
 	var current = Date();
-	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+	var seconds = (Date.parse(new Date()) - Date.parse("2020/07/05 12:15:30")) / 1000;
 	var days = Math.floor(seconds / (3600 * 24));
 	seconds = seconds % (3600 * 24);
 	var hours = Math.floor(seconds / 3600);
